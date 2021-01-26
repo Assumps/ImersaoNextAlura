8417be9a48b3d9d32260caf8e87a46c145ecc8b3
Assumps <jrvitor5@gmail.com>
2021-01-26 00:33:20 -0300

    insercao de hierarquia de estilos para cada componente e criando o front

--- with-styled-components-app/pages/index.js
+++ with-styled-components-app/pages/index.js
@@ -1,10 +1,59 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../pages/src/components/Widget'
+// function Title(props) { // propriedades ou propscidades 
+//   return (
+//   <h1>
+//     {props.children}
+//   </h1>
+//   )
+// }
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const BackgroundImage = styled.div`
+      background-image: url(${db.bg});
+      flex: 1;
+      background-size: cover;
+      background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top:450px;
+  margin: auto 10%;
+  @media screen and (max-width:500px){
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+   <BackgroundImage>
+     <QuizContainer>
+        <Widget>
+            <Widget.Header>
+              <h1>The Legends Of Coffe</h1>
+            </Widget.Header>
+
+
+          <Widget.Content>
+          <p>Lorem ipsum sit amet</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+
+          <h1>The Legends Of Coffe</h1>
+
+          <p>Lorem ipsum sit amet</p>
+
+          </Widget.Content>
+        </Widget>
+     </QuizContainer>
+   </BackgroundImage>
+
+    )
 }
